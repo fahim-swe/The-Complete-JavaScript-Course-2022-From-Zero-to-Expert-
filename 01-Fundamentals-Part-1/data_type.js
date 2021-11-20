89653175a2e50366f65d57e0d10519b7a24851a2
fahim <mdfahim1194@gmail.com>
2021-11-20 13:59:02 +0600

    JavaScript Fundamental

---
+++ 01-Fundamentals-Part-1/data_type.js
@@ -0,0 +1,9 @@
+// Data types
+/*
+JavaScript has dynamic typing: we do not have to manually define the data
+type of the value stored in a variable. Instead, data types are detemined
+automatically
+
+*/
+
+
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,39 @@
+// let js = "amazing";
+// if (js == "amazing") {
+//     alert("java Script is " + js);
+// }
+
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
+
+// values
+console.log("Hellow, Friends");
+console.log(23);
+console.log(50 + 10);
+
+
+
+// Variables camelcase(first word lowercase, and last word uppercase)
+// use a name that eassy to understand
+let firstName = "Fahim";
+console.log(firstName);
+firstName = "Abir, my friend";
+console.log(firstName);
+console.log(firstName);
+
+let firstNamePerson = "Abir Ahmed";
+let first_name_person = "Fahim islam";
+
+let Person = "Fahim"; // First Letter uppper case, Not recommonder, but specially use in some case
+let PI = 3.1416; // for const use uppercase
+
+let myFirstJob = "Programmer"; // Good practices
+let myCurrentJob = "Student";
+
+let job1 = "Programmer"; // bad practices
+let job2 = "Student";
+
+
+
+
+// Data type
